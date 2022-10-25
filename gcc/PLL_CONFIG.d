547cfb9f933cc34ba2c63807293b88038a190193
AlbertoGonzalezMoreno <113123601+AlbertoGonzalezMoreno@users.noreply.github.com>
2022-10-25 13:33:36 -0500

    Add files via upload

---
+++ gcc/PLL_CONFIG.d
@@ -0,0 +1,29 @@
+gcc/PLL_CONFIG.o: PLL_CONFIG.c lib/include.h \
+ d:\gnu\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h \
+ d:\gnu\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\stdlib.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\_ansi.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\newlib.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\config.h \
+ d:\gnu\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\reent.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\_ansi.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\_types.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\machine\_types.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\lock.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\cdefs.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\machine\stdlib.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\string.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\sys\string.h \
+ d:\gnu\10\ 2021.10\arm-none-eabi\include\math.h lib/TM4C123GH6PM.h \
+ lib/../CMSIS/Include/core_cm4.h lib/../CMSIS/Include/core_cmInstr.h \
+ lib/../CMSIS/Include/core_cmFunc.h lib/../CMSIS/Include/core_cm4_simd.h \
+ lib/../lib/system_TM4C123.h lib/GPIO.h lib/PLL_CONFIG.h \
+ lib/SysTick_Init.h
